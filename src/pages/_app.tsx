074ba689e9aaeb0438349aae28167fe4ba63cebe
suzukikoutarou <kotarosz727@gmail.com>
2023-03-20 22:59:38 +0900

    feat: app.tsxの修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+      <>
+        <Head>
+          <title>Next.js + TypeScript + Tailwind CSS</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+            {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />
+      </>
+  );
 }
